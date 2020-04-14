85a04ef85bff73a27c4097641f9c76b6a8dd899c
Dany <gamarra.dany@gmail.com>
2020-04-14 17:23:49 -0500

    Category: Repository, Service and Controller

--- src/main/java/com/ea/miushop/domain/Category.java
+++ src/main/java/com/ea/miushop/domain/Category.java
@@ -9,6 +9,7 @@ public class Category {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
+    @Column(nullable = false)
     private String categoryName;
 
     public Category() {
