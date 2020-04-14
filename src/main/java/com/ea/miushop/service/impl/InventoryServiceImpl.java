da6e110f5a30a9e7e3885ddd0651c8fad3307954
Dany <gamarra.dany@gmail.com>
2020-04-13 22:02:00 -0500

    Added Inventory: domain, servc., reposit.

---
+++ src/main/java/com/ea/miushop/service/impl/InventoryServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ea.miushop.service.impl;
+
+import java.util.List;
+
+import com.ea.miushop.domain.Inventory;
+import com.ea.miushop.service.InventoryService;
+
+public class InventoryServiceImpl implements InventoryService {
+
+	@Override
+	public List<Inventory> getAllInventory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Inventory getInventory(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void enterInventory(Inventory inventory) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateInventory(Inventory inventory) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
