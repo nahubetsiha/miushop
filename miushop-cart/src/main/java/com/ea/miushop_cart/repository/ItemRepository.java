9dd3a93b43c4d400904352ddd017241b3bdf19d5
mhaddis <54864661+mhaddis@users.noreply.github.com>
2020-04-14 06:58:58 -0500

    removed Item repo

--- miushop-cart/src/main/java/com/ea/miushop_cart/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ea.miushop_cart.repository;
-
-import com.ea.miushop_cart.model.OrderedItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<OrderedItem, Long> {
-}
